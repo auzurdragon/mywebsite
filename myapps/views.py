ea551d421bb40a2ba9ad11c94b4099497e827ddb
auzurdragon <auzurdragon@163.com>
2019-07-05 15:26:02 +0800

    1

--- myapps/views.py
+++ myapps/views.py
@@ -23,11 +23,14 @@ def qydata(request):
     pagenum = 1
     if keywords:
         from myapps.qydata import GetQyDataQichacha
-        qicc = GetQyDataQichacha(keywords.split())
-        qicc.get_list_all(pagenum)
-        RowNum = len(qicc.QyList)
-        if RowNum:
-            TableTitle = ['名称','状态','注册资本','成立时间','联系人','电话','邮箱','地址']
-            TableRow = [ [i['Name'],i['Status'],i['Capital'],i['estDate'],i['LxName'],i['Tel'],i['Email'],i['Addr']] for i in qicc.QyList]
+        try:
+            qicc = GetQyDataQichacha(keywords.split())
+            qicc.get_list_all(pagenum)
+            RowNum = len(qicc.QyList)
+            if RowNum:
+                TableTitle = ['名称','状态','注册资本','成立时间','联系人','电话','邮箱','地址']
+                TableRow = [ [i['Name'],i['Status'],i['Capital'],i['estDate'],i['LxName'],i['Tel'],i['Email'],i['Addr']] for i in qicc.QyList]
+        except Exception as e:
+            TableTitle, TableRow, RowNum = [],[], 0
     print(TableTitle, TableRow, RowNum)
     return render(request, 'search.html',{'Api':api,'TableTitle':TableTitle,'TableRow':TableRow, 'RowNum':RowNum,'description':description})
